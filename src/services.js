bbee5c88b947a4dcecd8947ed257687158b8359e
MacBook <macbook@MacBook-Pro-MacBook.local>
2019-09-18 21:22:52 +0300

    find news done

--- src/services.js
+++ src/services.js
@@ -1,20 +1,39 @@
 import axios from "axios";
 export default {
+  refs: {
+    next: document.querySelector(".next"),
+    container: document.querySelector(".container"),
+    qurty: document.querySelector(".qurty")
+  },
   image: [],
-  searchQuery: "",
+  searchQuery: "" ? "" : "javaScript",
   page: 1,
   key: "7c3a37959eb94dc390f27c80ee3fa21b",
   api: `https://newsapi.org/v2/everything?`,
   fetchNews: function(value) {
     console.log("value", value);
     console.log("searchQuery", this.searchQuery);
-    this.nextPage();
     console.log(this.page);
+    this.nextPage();
     return axios.get(
       `${this.api}q=${value}&apiKey=${this.key}&page=${this.page}`
     );
   },
+  defaultNews: function() {
+    console.log(this.page);
+    this.nextPage();
+    return axios.get(
+      `${this.api}q=${this.searchQuery}&apiKey=${this.key}&page=${this.page}`
+    );
+  },
+  alarm() {
+    if (this.page === 5) {
+      alert("alarm only 5 pages");
+    }
+  },
+
   nextPage() {
+    this.alarm();
     this.page += 1;
   },
   getValue(value) {
